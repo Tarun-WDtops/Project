919f546b09947d4c18f3ad223d70d2bdf020eef6
Tarun-WDtops <tjparmar786@gmail.com>
2024-12-02 10:06:14 +0530

    Estate website

--- Estate_Website/index.js
+++ Estate_Website/index.js
@@ -1,12 +1,15 @@
 function changeBg() {
   document.getElementById("btn1").style.backgroundColor = "white";
   document.getElementById("btn1").style.color = "black";
+  document.getElementById("btn2").style.backgroundColor = "#77c720";
+  document.getElementById("btn3").style.backgroundColor = "#77c720";
 }
 
 function changeBg1() {
   document.getElementById("btn2").style.backgroundColor = "white";
   document.getElementById("btn2").style.color = "black";
   document.getElementById("btn1").style.backgroundColor = "#77c720";
+  document.getElementById("btn3").style.backgroundColor = "#77c720";
 }
 
 function changeBg2() {
